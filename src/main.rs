0e1b2c0024daa96a134db04a35f3bcd058f8c778
Christopher Wilke <christopher.wilke86@googlemail.com>
2023-02-04 20:48:29 +0100

    added console-subscriber

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,15 @@ use log::{info, debug, error};
 #[tokio::main]
 pub async fn main() -> Result<(), anyhow::Error> {
 
+    console_subscriber::init();
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let cfg = config::read_cfg().await?;
     let state: StateManager = Arc::new(Mutex::new(HashMap::new()));
+    let cfg = config::read_cfg().await?;
+
+    // (1) Set addresses in hashhmap and wait for it to finish
+    // (2) Run scanner continuously
     
     loop {
         let iota_client = Client::builder()
@@ -44,12 +48,14 @@ async fn get_balance(
 
     for balance_address_response in resp {
        
-        let mut state = state.lock().unwrap();
-        state.insert(balance_address_response.address.clone(), BalanceState {
-            balance: balance_address_response.balance,
-            last_updated: SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs()
-        });
-
+        {
+            let mut state = state.lock().unwrap();
+            state.insert(balance_address_response.address.clone(), BalanceState {
+                balance: balance_address_response.balance,
+                last_updated: SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs()
+            });
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(3000)).await;
         info!("Updated State: {state:?}");
     }
     Ok(())
